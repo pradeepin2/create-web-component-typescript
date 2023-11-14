2ece8702e402dfb9e5ec31762ef7f5e6b8ae6b76
Pradeep Kumar Saraswathi <pradeepin2@gmail.com>
2023-11-13 18:01:04 -0800

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,2 +1,9 @@
+class MyFirstWebComponent extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `<h1>Hi Pradeep Kumar Saraswathi</h1>`;
+    }
+  }
+customElements.define('my-first-web-component', MyFirstWebComponent);
+
 var hello = 'Hello TS';
 document.body.innerHTML = `<div>${hello}</div>`
