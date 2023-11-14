292d37f7bed7c2aae84e358fd339a45e224e3646
Pradeep Kumar Saraswathi <pradeepin2@gmail.com>
2023-11-13 18:18:42 -0800

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,3 +1,2 @@
 import MyFirstWebComponent from "./MyFirstWebComponent"
-customElements.define('my-first-web-component', MyFirstWebComponent);
 document.body.innerHTML = `<my-first-web-component></my-first-web-component>`
